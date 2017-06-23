ef1988c7616f310ebe3e8ca5d3d13d00e8851b3e
slammayjammay <slammayjammay@gmail.com>
2017-06-23 10:53:55 -0700

    Add header search animation

--- src/js/components/HeaderComponent.vue
+++ src/js/components/HeaderComponent.vue
@@ -1,6 +1,6 @@
 <template>
-	<div class="header">
-		<div class="container">
+	<div class="header" :class="headerClass">
+		<div class="container" ref="container">
 			<div class="header-title vertically-center-container">
 				<h1 class="vertically-center">Medium</h1>
 			</div>
@@ -11,8 +11,13 @@
 				<li class="header-link header-link-sign-up vertically-center">
 					<a class="gray-link" href="#">Sign in / Sign up</a>
 				</li>
-				<li class="header-link header-link-search vertically-center">
-					<div class="search-icon"></div>
+				<li class="header-link header-link-search vertically-center-container vertically-center">
+					<div class="search-icon vertically-center" v-on:click="search"></div>
+						<input class="search-input vertically-center"
+							ref="input"
+							type="text"
+							placeholder="Search Medium"
+						/>
 				</li>
 			</ul>
 		</div>
@@ -20,13 +25,41 @@
 </template>
 
 <script>
-	export default {};
+	export default {
+		data() {
+			return {
+				isSearching: false
+			};
+		},
+		computed: {
+			headerClass() {
+				return this.isSearching ? 'is-searching' : '';
+			}
+		},
+		methods: {
+			search() {
+				this.isSearching = !this.isSearching;
+			},
+			onSearchTransitionEnd(e) {
+				if (this.isSearching) {
+					this.$refs.input.focus();
+				}
+			}
+		},
+		mounted() {
+			this.$refs.container.addEventListener('transitionend', this.onSearchTransitionEnd);
+		},
+		destroyed() {
+			this.$refs.container.removeEventListener('transitionend', this.onSearchTransitionEnd);
+		}
+	};
 </script>
 
 <style lang="scss" scoped>
 	$max-width: 1000px;
 	$height: 65px;
 	$background-color: #fff;
+	$search-input-width: 200px;
 
 	.header {
 		width: 100%;
@@ -36,6 +69,8 @@
 			max-width: $max-width;
 			height: $height;
 			margin: auto;
+			position: relative;
+			overflow: hidden;
 			border-bottom: 1px solid rgba(0, 0, 0, .05);
 
 			.header-title {
@@ -44,6 +79,7 @@
 
 				h1 {
 					margin: 0;
+
 					&:before {
 						height: 100%;
 					}
@@ -51,6 +87,8 @@
 			}
 
 			.header-links {
+				will-change: transform;
+				transition: 260ms ease;
 				float: right;
 				height: 100%;
 
@@ -75,6 +113,7 @@
 
 					&.header-link-search {
 						padding: 0;
+						margin-right: 0;
 
 						&:hover {
 							cursor: pointer;
@@ -92,9 +131,31 @@
 							background-image: url('../../../images/search-icon.png');
 							background-size: cover;
 							background-position: center;
+							margin-right: 5px;
+						}
+
+						.search-input {
+							position: absolute;
+							top: 0;
+							bottom: 0;
+							margin: auto;
+							height: 30px;
+							border: none;
+							font-size: 14px;
+							width: $search-input-width;
+
+							&:focus {
+								outline: none;
+							}
 						}
 					}
 				}
+			}
+		}
+
+		&.is-searching {
+			.header-links {
+				transform: translateX(-$search-input-width);
 			}
 		}
 	}
